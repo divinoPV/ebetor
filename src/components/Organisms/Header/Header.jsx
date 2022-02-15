2ace116e8918050c75e2efe9c5ed61abf5f5e9ea
divino <monteiro.hugo2001@icloud.com>
2022-02-15 21:19:55 +0100

    feature(#17)[display the information of the connected user]

--- src/components/Organisms/Header/Header.jsx
+++ src/components/Organisms/Header/Header.jsx
@@ -3,13 +3,22 @@ import React from 'react';
 import Logo from '../../Atoms/Logo/Logo';
 import Navbar from '../../Molecules/Navbar/Navbar';
 import Title from '../../Atoms/Title/Title';
+import { useAuthenticationStore } from '../../Trumps/Stores/Authentication/store';
 
-const Header = ({ active }) => <header>
-  <div>
-    <Logo url='/media/favicon_io/favicon.ico' alt='Logo du site Ebetor' />
-    <Title text='Ebetor' />
-  </div>
-  <Navbar active={active} />
-</header>;
+const Header = ({ active }) => {
+  const { authentication } = useAuthenticationStore();
+
+  return <header>
+    <div>
+      <Logo url="/media/favicon_io/favicon.ico" alt="Logo du site Ebetor" />
+      <Title text="Ebetor" />
+    </div>
+    {authentication.logged && <div>
+      <span>{authentication.name} | </span>
+      <span>{authentication.coins} coins</span>
+    </div>}
+    <Navbar active={active} />
+  </header>;
+};
 
 export default Header;
