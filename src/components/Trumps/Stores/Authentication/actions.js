b508e2c157d62e860ef10180bb2828e495035842
divino <monteiro.hugo2001@icloud.com>
2022-02-16 20:24:23 +0100

    feature(#19)[registration module]{authentication store}: add registration action

--- src/components/Trumps/Stores/Authentication/actions.js
+++ src/components/Trumps/Stores/Authentication/actions.js
@@ -1,14 +1,16 @@
 /* Actions */
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const REGISTRATION = 'REGISTRATION';
 export const LOGOUT = 'LOGOUT';
 
 /* Actions creators */
 export const loginSuccess = ({ id, name, email, coins }) => ({ type: LOGIN_SUCCESS, id, name, email, coins });
 export const loginFail = error => ({ type: LOGIN_FAIL, error });
+export const registration = ({ id, name, email, password }) => ({ type: REGISTRATION, id, name, email, password });
 export const logout = () => ({ type: LOGOUT });
 
-export const actions = { LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT };
-export const creators = { loginSuccess, loginFail, logout };
+export const actions = { LOGIN_SUCCESS, LOGIN_FAIL, REGISTRATION, LOGOUT };
+export const creators = { loginSuccess, loginFail, registration, logout };
 
 export default actions;
--- src/components/Trumps/Stores/Authentication/reducer.js
+++ src/components/Trumps/Stores/Authentication/reducer.js
@@ -17,6 +17,15 @@ const reducer = (state, action) => ({
     logged: false,
     error: action.error
   },
+  [actions.REGISTRATION]: {
+    // id: (async () => (await axios.get('http://localhost:3001/users'))?.data.slice(-1)[0].id + 1)(),
+    id: action.id,
+    name: action.name,
+    email: action.email,
+    coins: 0,
+    logged: false,
+    error: action.error
+  },
   [actions.LOGOUT]: {
     id: null,
     name: null,
