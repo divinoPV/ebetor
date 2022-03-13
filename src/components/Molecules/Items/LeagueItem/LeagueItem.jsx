5707f5616e57926e3923127e2e099482cff03e55
divino <monteiro.hugo2001@icloud.com>
2022-03-13 20:26:00 +0100

    [WIP] | feature(#28)[archive team and it cards]

--- src/components/Molecules/Items/LeagueItem/LeagueItem.jsx
+++ src/components/Molecules/Items/LeagueItem/LeagueItem.jsx
@@ -4,9 +4,11 @@ import styles from './LeagueItem.module.scss';
 const LeagueItem = ({ alt, children, className, id, name, src, ...rest }) =>
   <div className={`${styles['LeagueItem']}`}>
     <strong className={`${styles['LeagueItem__title']}`}>{name}</strong>
-    <div className={`${styles['LeagueItem__image']}`}>
-      <ImageItem alt={alt} className={className} src={src} {...rest} />
-    </div>
+    {src &&
+      <div className={`${styles['LeagueItem__image']}`}>
+        <ImageItem alt={alt} className={className} src={src} {...rest} />
+      </div>
+    }
     {children}
   </div>
 ;
--- src/components/Pages/Team/Teams.jsx
+++ src/components/Pages/Team/Teams.jsx
@@ -1,19 +1,52 @@
 import { useEffect, useState } from 'react';
 import axios from '../../../utils/axios';
+import ReactPaginate from 'react-paginate';
 
 import Header from '../../Organisms/Header/Header';
 import Main from '../../Organisms/Main/Main';
+import ItemsContainer from '../../Organisms/Container/ItemsContainer/ItemsContainer';
+import TeamItem from '../../Molecules/Items/TeamItem/TeamItem';
 
 const Teams = () => {
   const [teams, setTeams] = useState([]);
+  const [pageCount, setPageCount] = useState(1);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const itemsPerPage = 30;
 
   useEffect(() => {
-    (async () => setTeams((await axios.get('teams?page=1&per_page=50'))?.data))();
-  }, []);
+    axios.get(`teams?sort=name&page=${pageCount}&per_page=${itemsPerPage}`).then(response => setTeams(response.data));
+    setPageCount(pageCount + 1);
+  }, [itemOffset, itemsPerPage]);
+
+  const handlePageClick = event => {
+    setItemOffset(event.selected + 1);
+  };
+
+  console.log(teams);
 
   return <>
     <Header active="teams" />
-    <Main></Main>
+    <Main>
+      <ItemsContainer>
+        {teams && teams.map(team =>
+          <TeamItem key={team.id}
+                    alt={`Logo de la team ${team.name}`}
+                    team={team}
+                    src={team.image_url}
+          />
+        )}
+      </ItemsContainer>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="suivant"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="précédent"
+        renderOnZeroPageCount={null}
+      />
+    </Main>
   </>;
 };
 
